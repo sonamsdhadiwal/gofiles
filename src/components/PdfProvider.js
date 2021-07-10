822b5ace6a356c2f146d1f4144b99975f0000ede
sonamsd <ssdhadiwal.it@gmail.com>
2021-07-10 14:26:22 -0500

    working pdf merge functionality

--- src/components/PdfProvider.js
+++ src/components/PdfProvider.js
@@ -1,5 +1,4 @@
-///////////////
-//import { PDFDocumentFactory, PDFDocumentWriter } from 'pdf-lib';
+import { PDFDocumentFactory, PDFDocumentWriter } from 'pdf-lib';
 
 function mergeBetweenPDF(pdfFileList) {
 let returnObj = { pdfFile: null, pdfNotMergedList: [] }
@@ -7,10 +6,9 @@ let returnObj = { pdfFile: null, pdfNotMergedList: [] }
     if (pdfFileList.length > 0) {
 		const reader = new FileReader();
 		reader.onload = function(evt) { console.log(evt.target.result); };
-//////////////////
-		// const pdfDoc = PDFDocumentFactory.create();
-		// let iterPdfDoc = PDFDocumentFactory.create();
-		// let iterPdfDocBytes;
+		const pdfDoc = PDFDocumentFactory.create();
+		let iterPdfDoc = PDFDocumentFactory.create();
+	
 		let pdfFilePromiseArrayBufferList = [];
 		let pdfNotMergedList = [];
 		let iterPdfDocPages = [];
@@ -28,30 +26,29 @@ let returnObj = { pdfFile: null, pdfNotMergedList: [] }
 			.all(pdfFilePromiseArrayBufferList)
 			.then((pdfArrayBufferFileList) => {
 				for (let i = 0; i < pdfArrayBufferFileList.length; i++) {
-					// console.log(pdfArrayBufferFileList[i])
+					console.log(pdfArrayBufferFileList[i])
 					// iterPdfDocBytes = reader.readAsArrayBuffer(pdfBlob)
-					// try {
+					try {
 						
-					// 	iterPdfDoc = PDFDocumentFactory.load(new Uint8Array(pdfArrayBufferFileList[i]))
-					// 	iterPdfDocPages = iterPdfDoc.getPages()
-					// 	iterPdfDoc = PDFDocumentFactory.create();
-					// 	// Add each page in a temp file to check if all pages from this PDF can be added in the final one
-					// 	iterPdfDocPages.forEach((pdfPage) => {
-					// 		iterPdfDoc.addPage(pdfPage)
-					// 	})
-					// 	// No errors? Then add all pages to the final PDF
-					// 	iterPdfDocPages.forEach((pdfPage) => {
-					// 		pdfDoc.addPage(pdfPage)
-					// 	})
-					// } catch (err) {
-					// 	console.log(err)
-					// 	// console.log("File " + pdfFileList[i].name + " not merged due to the following error: " + err.message)
-					// 	pdfNotMergedList.push(pdfFileList[i].name)
-					// }
+						iterPdfDoc = PDFDocumentFactory.load(new Uint8Array(pdfArrayBufferFileList[i]))
+						iterPdfDocPages = iterPdfDoc.getPages()
+						iterPdfDoc = PDFDocumentFactory.create();
+						// Add each page in a temp file to check if all pages from this PDF can be added in the final one
+						iterPdfDocPages.forEach((pdfPage) => {
+							iterPdfDoc.addPage(pdfPage)
+						})
+						// No errors? Then add all pages to the final PDF
+						iterPdfDocPages.forEach((pdfPage) => {
+							pdfDoc.addPage(pdfPage)
+						})
+					} catch (err) {
+						console.log(err)
+						// console.log("File " + pdfFileList[i].name + " not merged due to the following error: " + err.message)
+						pdfNotMergedList.push(pdfFileList[i].name)
+					}
 				}
-//////////
-				// returnObj.pdfFile = PDFDocumentWriter.saveToBytes(pdfDoc)
-				// returnObj.pdfNotMergedList = pdfNotMergedList
+				returnObj.pdfFile = PDFDocumentWriter.saveToBytes(pdfDoc)
+				returnObj.pdfNotMergedList = pdfNotMergedList
 				return returnObj
 			})
 			.catch((err) => {
