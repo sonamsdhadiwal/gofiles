365a547dcccf89646fa4be747619f568aef5112f
sonamsd <ssdhadiwal.it@gmail.com>
2021-07-13 19:44:08 -0500

    code changes

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -21,7 +21,6 @@ const Styles = styled.div`
 export const NavigationBar = () => (
   <Styles>
     <Navbar expand="lg">
-      <Navbar.Brand href="/">Tutorial</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav"/>
       <Form className="form-center">
         <FormControl type="text" placeholder="Search" className="" />
